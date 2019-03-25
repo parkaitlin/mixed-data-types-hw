3ca1c9a46ecc6c4b2a10296fd4abe4c980a76503
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-03-25 16:00:51 -0700

    Data Structures

--- apps.js
+++ apps.js
@@ -12,4 +12,27 @@
 // A list of student names from our class, each with a location and each with a list of favorite tv shows.
     // data types: 
 
-    
+// 2. Take it Easy
+    // 1.
+    const theRainbow = [
+        'red',
+        'orange',
+        'yellow',
+        'green',
+        'blue',
+        'purple'
+    ];
+    // 2. 
+    console.log(theRainbow[4]);
+    // 3.
+    const khp = {
+        name: 'Kaitlin H Park',
+        faveFood: 'Fried Chicken & Beer',
+        hobby: 'golf',
+        currentHometown: 'Orange County, CA',
+        faveDataType: 'Objects'
+    };
+    // 4.
+    console.log(khp.hobby);
+
+
