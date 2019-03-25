e4df90338a8a1e7d12b55b0da3d56863a593da8c
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-03-25 15:49:11 -0700

    Commit 1 - Data Types

---
+++ apps.js
@@ -0,0 +1,15 @@
+// 1. Data types
+// A light switch that can be either on or off
+    // data types: 
+// A user's email address
+    // data types:
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive
+    // data types:
+// A list of student names from our class
+    // data types:
+// A list of student names from our class, each with a location
+    // data types:
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+    // data types: 
+
+    
